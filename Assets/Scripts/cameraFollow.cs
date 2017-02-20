4bb0874bdf5115c355bf27076028e6176f84befc
Emonatte <chlu0024@student.hv.se>
2017-02-20 16:06:20 +0100

    te3s

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+
+    public Transform target;
+    public float m_speed = 0.1f;
+    Camera mycam;
+
+	// Use this for initialization
+	void Start () {
+        mycam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mycam.orthographicSize = (Screen.height / 50f / 1f);
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, m_speed);
+        }
+	}
+}
